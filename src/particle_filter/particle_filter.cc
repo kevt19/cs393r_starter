ec0b96814b499307c3122092f40e804484cc4d7d
amrl_user <matkozlowski@utexas.edu>
2024-03-03 15:38:07 -0600

    Small fix for calculating weight

--- src/particle_filter/particle_filter.cc
+++ src/particle_filter/particle_filter.cc
@@ -81,7 +81,7 @@ void ParticleFilter::GetPredictedPointCloud(const Vector2f& loc,
                                             vector<Vector2f>* scan_ptr,
                                             vector<double>* ranges_ptr) {
 
-  float num_ranges = num_ranges_full/47; // Creates 23 points out of 1081
+  float num_ranges = num_ranges_full/23; // Creates 47 points out of 1081
   vector<Vector2f>& scan = *scan_ptr;
   scan.resize(num_ranges);
 
@@ -157,7 +157,7 @@ void ParticleFilter::Update(const vector<float>& ranges,
   vector<double> predicted_ranges;
   GetPredictedPointCloud(p_ptr->loc, p_ptr->angle, ranges.size(), range_min, range_max, angle_min, angle_max, &predicted_scan, &predicted_ranges);
   double log_likelihood = 0;
-  for (size_t i = 0; i < ranges.size(); i += 47)
+  for (size_t i = 0; i < ranges.size(); i += 23)
   {
     if (ranges[i] < range_min && ranges[i] > range_max){
       log_likelihood += 0;
@@ -177,7 +177,7 @@ void ParticleFilter::Update(const vector<float>& ranges,
 
     // log_likelihood +=  FLAGS_gamma * (-0.5) * (pow(ranges[i] - predicted_ranges[i], 2) / pow(FLAGS_stddev, 2));  // Simple Version
   }
-  p_ptr->weight = 1 / abs(log_likelihood);
+  p_ptr->weight = 1.0 / abs(log_likelihood);
 }
 
 void ParticleFilter::Resample() {
